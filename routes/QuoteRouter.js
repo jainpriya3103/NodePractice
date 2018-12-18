bd9d4a2acab0f6b3321d8c5eaed875a7d98e15ff
Priya <jainpriya3103@gmail.com>
2018-12-18 22:29:41 +0530

    commit

--- routes/QuoteRouter.js
+++ routes/QuoteRouter.js
@@ -1,17 +1,19 @@
 const express = require('express');
+
 const QuoteRouter = express.Router();
+
 const bodyParser = require('body-parser');
 QuoteRouter.use(bodyParser.urlencoded({extended: true}));
 QuoteRouter.use(bodyParser.json());
+
 const dbUrl = require('../common/dbconfig').dbUrl;
 const dbName = require('../common/dbconfig').dbName;
-
 const MongoClient = require('mongodb');
+var ObjectId = require('mongodb').ObjectID;
 var db;
-
 MongoClient.connect(dbUrl, (err, client) => {
   if (err) return console.log(err)
-  db = client.db(dbName) // whatever your database name is
+  db = client.db(dbName) 
 });
 
 
@@ -37,8 +39,39 @@ QuoteRouter.post('/post', (req, res) => {
     if (err) return console.log(err)
 
     console.log('saved to database')
-    res.redirect('/')
+    res.redirect('/quotes')
   })
+});
+
+QuoteRouter.route('/edit/:id').get(function (req, res) {
+  db.collection('quotes').findOne({"_id": ObjectId(req.params.id.toString()) })
+  .then(function (result){
+      console.log(result);
+      res.render('edit', {quoteObj: result});
+  });
+});
+
+QuoteRouter.post('/update/:id', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({"_id": ObjectId(req.params.id.toString()) }, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.redirect('/quotes');
+  })
+});
+
+QuoteRouter.route('/delete/:id').get(function (req, res) {
+  db.collection('quotes').deleteOne({"_id": ObjectId(req.params.id.toString()) })
+  .then(function (){
+    res.redirect('/quotes');
+  });
 });
 
 module.exports = QuoteRouter;
