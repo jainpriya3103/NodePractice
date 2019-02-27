f5801dbb8b7f9acb4926de47e0ee024c56343324
Priya Jain <jainpriya3103@gmail.com>
2019-02-27 20:11:55 +0530

    test

--- common/today.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "ns-today",
-    templateUrl: "./today.component.html",
-    styleUrls: ["./today.component.css"],
-    moduleId: module.id
-  })
-export class TodayComponent{}
